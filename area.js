fea202e564a01440be85211f4743be052c237d60
Bikarna Pokharel <bikarna.pokharel@tuni.fi>
2019-12-07 23:50:43 +0200

    Refactored area.js/Debug

--- area.js
+++ area.js
@@ -2,23 +2,32 @@
 
 var fs = require('fs');
 var vector3 = require('vector-3');
+
 var vertices = [['0', '0', '0']];
+
 var xCoord = [0];
 var yCoord = [0];
 var zCoord = [0];
-var vertexA = "", vertexB = "", vertexC = "";
-var totalSurfaceArea = 0;
 
-fs.readFile(process.argv[2], 'utf-8', (error, data) => {
-    var tokens = data.split(/\s+/);    
+var vertexA = "";
+var vertexB = ""; 
+var vertexC = "";
+
+var totalSurfaceArea = 0;
+var file = process.argv;
+var readFile = fs.readFile(file[2], 'utf8', (error, data) => {
+    if(error) console.log(error.name, error.message);
+    var tokens = data.split(/\s+/); 
+
     var fposV = tokens.indexOf('v');
     var fposF = tokens.indexOf('f');
 
     for(var i = fposV; i < tokens.length; i++) {
-        if(tokens[i] === 'v')
+        if(tokens[i] == 'v')
             vertices.push([tokens[i+1], tokens[i+2], tokens[i+3]]);
         
     }
+    //console.log(vertices.length);
 
     for(var j = fposF; j < tokens.length; j++) {
         if(tokens[j] === 'f') {
@@ -28,20 +37,21 @@ fs.readFile(process.argv[2], 'utf-8', (error, data) => {
                     yCoord[1] = parseFloat(vertices[k][1]);
                     zCoord[1] = parseFloat(vertices[k][2]);
                     vertexA = new vector3(xCoord[1], yCoord[1], zCoord[1]);
+                    //console.log(vertexA)
                 }
 
                 if(tokens[ j + 2 ] == k) {
                     xCoord[2] = parseFloat(vertices[k][0]);
                     yCoord[2] = parseFloat(vertices[k][1]);
                     zCoord[2] = parseFloat(vertices[k][2]);
-                    vertexB = new vector3(xCoord[1], yCoord[1], zCoord[1]);
+                    vertexB = new vector3(xCoord[2], yCoord[2], zCoord[2]);
                 }
 
                 if(tokens[ j + 3 ] == k) {
                     xCoord[3] = parseFloat(vertices[k][0]);
                     yCoord[3] = parseFloat(vertices[k][1]);
                     zCoord[3] = parseFloat(vertices[k][2]);
-                    vertexC = new vector3(xCoord[1], yCoord[1], zCoord[1]);
+                    vertexC = new vector3(xCoord[3], yCoord[3], zCoord[3]);
                 }
 
                 if(vertexA != "" && vertexB != "" && vertexC != ""){
@@ -68,21 +78,18 @@ fs.readFile(process.argv[2], 'utf-8', (error, data) => {
 
                     totalSurfaceArea += areaOfATriangle(lengthAB, lengthAC, lengthBC);
 
-          //totalSurfaceArea += areaOfATriangle(vectorAB, vectorAC);
-
-                    vertexA = vertexB = vertexC = ""; //vertices A, B, and C reinitialized to empty
-          // otherwise the mixed vertices map to faces
+                    vertexA = vertexB = vertexC = ""; 
 
                 } 
             }
         }
     }
-  console.log(totalSurfaceArea)  
+  console.log(totalSurfaceArea);
 })
 
 
-function areaOfATriangle (a, b, c) {
-    var s = (a + b + c) / 2
-    var area = (s * (s - a)*(s - b)*(s - c))
-    return area
+const areaOfATriangle = (a, b, c) => {
+    var s = (a + b + c) / 2;
+    var area = (s * (s - a)*(s - b)*(s - c))**(1/2);
+    return area;
 }
